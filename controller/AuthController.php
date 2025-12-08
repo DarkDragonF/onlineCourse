7d89b0f019641e0603492e8a71fd343eb9c4c4d6
nta.dev <thaianhnguyenfriends@gmail.com>
2025-12-08 18:00:58 +0700

    small changes

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -16,33 +16,36 @@ class AuthController{
 
     public function handleLogin(){
         if($_SERVER['REQUEST_METHOD'] = 'POST'){
-            $email = $_POST['email'] ?? '';
-            $password = $_POST['password'] ?? '';
+            $email = htmlspecialchars($_POST['email']) ?? '';
+            $password = htmlspecialchars($_POST['password']) ?? '';
 
             $user = $this->userModel->getUserByEmail($email);
+            $error = [];
+            //validate dữ liệu
             if($user && password_verify($password, $user['password'])){
                 //Lưu session
-            if(session_status() = PHP_SESSION_NONE){
-                $_SESSION['user_id'] = $user['id'];
-                $_SESSION['user_name'] = $user['fullname'];
-                $_SESSION['role'] = $user['role'];
-            }
-            //Xét userRole
-            if($user['role'] == 0){
-                header('Location: ...');
-                exit;
-            }
-            else if($user['role'] == 1){
-                header('Location: ...');
-                exit;
-            }
-            else if($user['role'] == 2){
-                header('Location: ...');
-                exit;
-            }
+                if(session_status() == PHP_SESSION_NONE){
+                    $_SESSION['user_id'] = $user['id'];
+                    $_SESSION['user_name'] = $user['fullname'];
+                    $_SESSION['role'] = $user['role'];
+                }
+                
+                //Xét userRole
+                if($user['role'] == 0){
+                    header('Location: ...');
+                    exit;
+                }
+                else if($user['role'] == 1){
+                    header('Location: ...');
+                    exit;
+                }
+                else if($user['role'] == 2){
+                    header('Location: ...');
+                    exit;
+                }
             }
             else{
-                $error = "Email hoặc mật khẩu không chính xác!";
+                $error = "...";
                 //require view của login để nhìn thấy biến $error
                 require "/views/auth/login.php";
             }
@@ -55,13 +58,14 @@ class AuthController{
     }
 
     public function handleRegister(){
-        if($_SERVER['REQUEST_METHOD'] = 'POST'){
-            $userName = $_POST['username'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $fullname = $_POST['fullname'];
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $userName = htmlspecialchars($_POST['username']);
+            $email = htmlspecialchars($_POST['email']);
+            $password = htmlspecialchars($_POST['password']);
+            $fullname = htmlspecialchars($_POST['fullname']);
 
             $role = 0;
+            //validate dữ liệu
 
             $isCreated = $this->userModel->studentRegister($userName, $email, $password, $fullname);
 
@@ -69,7 +73,7 @@ class AuthController{
                 header('Location: ....');
             }
             else{
-                $error = 'Lỗi đăng ký!';
+                $error = 'Lỗi!';
             }
         }
     }
